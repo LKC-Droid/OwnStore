56978c4ef32af94b9ee6b2ec321f294a3102ff02
LKC-Droid <karel.goldenberg10@gmail.com>
2019-12-26 01:42:11 -0300

    Actualizacion

--- app/src/main/java/com/lkcdroid/ownstore/ui/VistasCRUD/MyShopActivity.java
+++ app/src/main/java/com/lkcdroid/ownstore/ui/VistasCRUD/MyShopActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.lkcdroid.ownstore.R;
@@ -18,12 +19,19 @@ public class MyShopActivity extends AppCompatActivity {
     private EditText editNombre;
     private EditText editDescripcion;
     private EditText editDireccion;
+    private ImageButton imBtnLogo;
     private Button btnIngresar;
     private TiendaDAL tiendaDAL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_shop);
+
+        this.editNombre=(EditText) findViewById(R.id.txtNombre);
+        this.editDescripcion=(EditText) findViewById(R.id.txtDecripcion);
+        this.editDireccion=(EditText) findViewById(R.id.txtDireccion);
+        this.imBtnLogo=(ImageButton) findViewById(R.id.imBtnLogo);
+        this.btnIngresar=(Button) findViewById(R.id.btnCrear);
 
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
